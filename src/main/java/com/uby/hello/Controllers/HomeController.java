66a2c703a1e4416b24b3d9a02b5710a5da8b8fc1
Mehmet Kasım Sular <mksular@gmail.com>
2023-07-29 16:30:47 +0300

    RestController Kullanılarak Hello World Yazdırıldı

---
+++ src/main/java/com/uby/hello/Controllers/HomeController.java
@@ -0,0 +1,13 @@
+package com.uby.hello.Controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    @GetMapping("/")
+    public String helloWorld() {
+        return "Hello World";
+    }
+    
+}
